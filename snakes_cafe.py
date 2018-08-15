bc30781097575f24a25f10461a22d1bc13884f9e
Jason Burns <jasonb315@protonmail.com>
2018-08-14 19:57:31 -0700

    menu printout and order tally

--- snakes_cafe.py
+++ snakes_cafe.py
@@ -12,94 +12,150 @@ import sys
 
 WIDTH = 96
 
+
 # SEAT = [
 #     {
 #         'question': 'How many today?\n',
 #         'answer': '0',
 #     },
 # ]
-MENU = [APP, ENT, DES, BEV]
 
 APP = [
     {
-        'item:': 'Tarantula Crisp',
+        'header': 'APPETIZERSss',
+    },
+    {
+        'item': 'Tarantula Crisp',
         'count': 0,
     },
     {
-        'item:': 'Crickett Platter',
+        'item': 'Crickett Platter',
         'count': 0,
     },
     {
-        'item:': 'Mouse Kabob',
+        'item': 'Mouse Kabob',
         'count': 0,
     },
 ]
 
 ENT = [
     {
-        'item:': 'Roadkill',
+        'header': 'ENTRESss',
+    },
+    {
+        'item': 'Roadkill',
         'count': 0,
     },
     {
-        'item:': 'Brazed Housecat',
+        'item': 'Brazed Housecat',
         'count': 0,
     },
     {
-        'item:': 'Human Infant',
+        'item': 'Human Infant',
         'count': 0,
     },
 ]
 
 DES = [
     {
-        'item:': 'Mice-cream',
+        'header': 'DESSSERTSss',
+    },
+    {
+        'item': 'Mice-cream',
         'count': 0,
     },
     {
-        'item:': 'Frog-yo',
+        'item': 'Frog-yo',
         'count': 0,
     },
     {
-        'item:': 'Oreos and Cream',
+        'item': 'Rabbit Souffle',
         'count': 0,
     },
 ]
 
 BEV = [
     {
-        'item:': 'Rattlesnake',
+        'header': 'BEVERAGESss',
+    },
+    {
+        'item': 'Rattlesnake',
         'count': 0,
     },
     {
-        'item:': 'Snake Eyes',
+        'item': 'Snake Eyes',
         'count': 0,
     },
     {
-        'item:': 'Milk Snake',
+        'item': 'Milk Snake',
         'count': 0,
     },
 ]
 
-# close menu
+MENU = [APP, ENT, DES, BEV]
 
 
 def ask_question(question):
     return input(question)
 
 
-def check_input(user_in, item):
+def take_order():
+    initial_order = True
+    ordering = True
+    no = {'no', 'n', 'nay', 'no thanks', 'jog on mate', 'that\'s all'}
+
+    while ordering:
+        if initial_order:
+            item = input('What can I get ya?').lower()
+        else:
+            item = input('Anything else?').lower()
+
+        if item == no:
+            pass
+
+        else:
+            check_input(item)
+
+            for i in range(len(MENU)):
+                for j in range(len(MENU[i])-1):
+                    if(MENU[i][j+1]['item']).lower() == item:
+                        MENU[i][j+1]['count'] += 1
+                        print(dedent(f'''
+                            Ok, that's going to be {str(MENU[i][j+1]['count'])} {str(MENU[i][j+1]['item'])}
+                        '''))
+                        initial_order = False
+                        take_order()
+                    else:
+                        ordering = False
+
+
+def check_input(user_in):
     if user_in.lower() == 'leave':
         exit()
-        return
 
-    if user_in.lower()
+
+def order_ready():
+    yes = {'yes', 'y', 'ye', 'sure', 'Fuck yeah dawg', ''}
+    ready = input('Ready to order?')
+    if ready in yes:
+        take_order()
+    else:
+        print('look we\'re busy so make up your mind.')
+        order_ready()
 
 
 def give_menu():
-    for i in range(MENU):
-        print(MENU[i])
-        for e in MENU[i]:
-            print(e['item'])
+
+    for i in range(len(MENU)):
+
+        print(dedent(f'''
+            {'-' * WIDTH}
+            {MENU[i][0]['header']}
+            {'-' * WIDTH}
+        '''))
+        for j in range(len(MENU[i])-1):
+            print(MENU[i][j+1]['item'])
+        print('-' * WIDTH)
 
 
 def greeting():
@@ -111,7 +167,8 @@ def greeting():
 
     print(dedent(f'''
         {'*' * WIDTH}
-        {(' ' * ((WIDTH - len(ln_one)) // 2)) + ln_one + (' ' * (WIDTH - len(ln_one)) // 2))}
+        {(' ' * ((WIDTH - len(ln_one)) // 2)) + ln_one +
+            (' ' * ((WIDTH - len(ln_one)) // 2))}
         {'*' * WIDTH}
     '''))
 
@@ -125,6 +182,7 @@ def exit():
 def run():
     greeting()
     give_menu()
+    order_ready()
 
 
 if __name__ == '__main__':
